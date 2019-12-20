97f0c4f1d36dff9a2d88a66d6276026fd9a4d7c4
Dan Federman <dfed@me.com>
2019-12-20 09:49:10 -0800

    Add Package.swift to add Swift Package Manager compability

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftTryCatch",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5),
+        .macOS(.v10_14),
+    ],
+    products: [
+        .library(
+            name: "SwiftTryCatch",
+            targets: ["SwiftTryCatch"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftTryCatch",
+            dependencies: [],
+            path: "./",
+            publicHeadersPath: "./")
+    ]
+)
+let version = Version(1, 0, 0)
